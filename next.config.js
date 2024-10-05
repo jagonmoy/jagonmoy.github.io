b691ab63da48430b47fae515c37c39eca6ddf851
Jagonmoy <jagonmoy@subrite.no>
2024-10-05 07:47:31 +0600

    add gap

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.shields.io',
+        port: '',
+        pathname: '/badge/*',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
