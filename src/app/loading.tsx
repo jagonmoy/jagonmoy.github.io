66bb3e3f62bdfa257ebb01b8d8179eacb501d553
jagonmoy <jagonmoydey1997@gmail.com>
2024-10-23 02:54:32 +0600

    fix build error

---
+++ src/app/loading.tsx
@@ -0,0 +1,10 @@
+import Loader from 'components/Loader';
+
+function Loading() {
+  return (
+    <div className="justify-center items-center text-center">
+      <Loader />
+    </div>
+  );
+}
+export default Loading;
